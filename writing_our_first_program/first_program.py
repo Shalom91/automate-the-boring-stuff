78c5a267fbec7788902abbfb20a95d2085d7530e
Shalom91 <shalom.mothapo@gmail.com>
2021-06-04 12:05:45 +0200

    initial commit

---
+++ writing_our_first_program/first_program.py
@@ -0,0 +1,11 @@
+print("Hello world")
+print("what is your name?")
+myname = input()
+print("Its good to meet you " + myname)
+print("The length of your name is: ")
+print(len(myname))
+print("what is your age? ")
+myage = int(input())
+print("You will be " + str((myage + 1)) + " years old next year.")
+
+
